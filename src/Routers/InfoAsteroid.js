c6db655b5881c822018476239e67933ce2df8d7d
Sol-22 <sol.noeli.lopez@gmail.com>
2017-10-20 12:15:17 +0200

    add footer

--- src/Routers/InfoAsteroid.js
+++ src/Routers/InfoAsteroid.js
@@ -54,7 +54,7 @@ class InfoAsteroid extends Component {
                     <h4 className="text-center">{this.state.name}</h4>
                   	<p>{`The estimated size of this Asteroid ranges from ${this.state.estimated_diameter_min} Km, until the ${this.state.estimated_diameter_max} Km`}</p>
 					<p>{`Travel at a speed of ${this.state.velocity} Km/seg`}</p>
-					<p>{`The closest date to Earth was ${this.state.approach}and happened to ${this.state.distance_min} Km`}</p>
+					<p>{`The closest date to Earth was ${this.state.approach} and happened to ${this.state.distance_min} Km`}</p>
 					<p>{`The celestial body on which orbit is: ${this.state.orbiting_body}`}</p>
 					<p>{label}</p>
 					<Link to='/asteroid'  className="btn">Back</Link>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,22 @@ import React from 'react'
 import '../styles/Footer.css'
 const Footer = () => (
 	<footer>
-		<p>Copyright © 2017, All Rights Reserverd SkylabCoders</p>
-	</footer>
+		<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="copyright">
+				<p>Copyright © 2017, All Rights Reserverd SkylabCoders</p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div>
+					<a  href="https://github.com/emiliovillu">Emilio Villuendas <i class="fa fa-github" aria-hidden="true"></i></a>   |   <a  href="https://github.com/sol-22">Soledad López <i class="fa fa-github" aria-hidden="true"></i></a>   |    <a  href="https://github.com/sergi145">Sergi Picazo <i class="fa fa-github" aria-hidden="true"></i></a> 
+
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 )
 
 export default Footer
